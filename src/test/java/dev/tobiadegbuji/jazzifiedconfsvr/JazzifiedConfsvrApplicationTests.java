5aeec735a020c25c180911dbda29c2456fdd87cd
Tobi Adegbuji <adegbuji_tobi@yahoo.com>
2021-04-29 12:52:41 -0400

    Initial commit

---
+++ src/test/java/dev/tobiadegbuji/jazzifiedconfsvr/JazzifiedConfsvrApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tobiadegbuji.jazzifiedconfsvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazzifiedConfsvrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
